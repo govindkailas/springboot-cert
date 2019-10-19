2dbe45b33d89cd27674f92827fa0ece5b4e35560
Govind <govindkailas@gmail.com>
2019-10-19 13:29:26 +0530

    some of the java images doesnt have /jre path

--- add_certs_keytools.sh
+++ add_certs_keytools.sh
@@ -6,5 +6,7 @@ for URL in $(echo ${DOWNLOAD_URLS})
     do
 		echo "Fetching certificate from $URL"
 		echo -n | openssl s_client -connect ${URL}:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ${JAVA_HOME}/jre/lib/security/${URL}.cert
+		echo -n | openssl s_client -connect ${URL}:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ${JAVA_HOME}/lib/security/${URL}.cert
 		${JAVA_HOME}/bin/keytool -import -noprompt  -storepass changeit -alias ${URL} -keystore ${JAVA_HOME}/jre/lib/security/cacerts -file ${JAVA_HOME}/jre/lib/security/${URL}.cert
+		${JAVA_HOME}/bin/keytool -import -noprompt  -storepass changeit -alias ${URL} -keystore ${JAVA_HOME}/lib/security/cacerts -file ${JAVA_HOME}/lib/security/${URL}.cert
 done
